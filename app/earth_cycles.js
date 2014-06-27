0c99e2ed8c7137ff8281579d1c7d9018f32dcd02
JayChetty <jaypaulchetty@gmail.com>
2014-06-27 14:20:27 +0100

    earth cycles showing state

--- app/earth_cycles.js
+++ app/earth_cycles.js
@@ -13,12 +13,51 @@ var EarthCycles = function(){
 
   this.YEAR_IN_MOONTHS = (this.YEAR_IN_SECONDS/this.MOONTH_IN_SECONDS);
 
-  this.year = new Cycle(this.AN_AXIS_MAX_N,this.YEAR_IN_MILLISECONDS);
-  this.moonth = new Cycle(this.A_NEW_MOON, this.MOONTH_IN_MILLISECONDS);
+  this.year = new Cycle(this.AN_AXIS_MAX_N.valueOf(),this.YEAR_IN_MILLISECONDS);
+  this.moonth = new Cycle(this.A_NEW_MOON.valueOf(), this.MOONTH_IN_MILLISECONDS);
 
   this.yearsPassed = function(pointInTime){
-    return (this.year.cyclesSinceAnchor(pointInTime))
+    return (this.year.cyclesSinceAnchor(pointInTime));
   }
+
+  this.dayOfYear = function(pointInTime){
+    return (this.year.dayOfCycle(pointInTime));
+  }
+
+  this.dayOfMoonth = function(pointInTime){
+    return (this.moonth.dayOfCycle(pointInTime));
+  }
+
+  this.firstNewMoonOfYear = function(pointInTime){
+    var startOfYear = this.year.startOfCurrentCycle(pointInTime);
+    return (this.moonth.startOfNextCycle(startOfYear));
+  }
+
+  this.firstNewDayOfMoonth = function(pointInTime){
+    return (this.moonth.firstDayStartOfCurrentCycle(pointInTime));
+  }
+
+  this.moonthOfYear = function(pointInTime){
+    var adjustedTime = pointInTime - this.firstNewMoonOfYear(pointInTime);
+    return (Math.ceil(adjustedTime/this.MOONTH_IN_MILLISECONDS) + 1);
+  }
+
+
+  this.moonthsInYear = function(pointInTime){
+    var firstMoonthLag = this.firstNewMoonOfYear(pointInTime) - this.year.startOfCurrentCycle(pointInTime);
+    return Math.ceil((this.YEAR_IN_MILLISECONDS - firstMoonthLag)/this.MOONTH_IN_MILLISECONDS);
+  }
+
+  this.daysInMoonth = function(pointInTime){
+    var firstDayMoonthLag = this.firstNewDayOfMoonth(pointInTime) - this.moonth.startOfCurrentCycle(pointInTime);
+    return Math.ceil((this.MOONTH_IN_MILLISECONDS - firstDayMoonthLag)/this.DAY_IN_MILLISECONDS);
+  }
+
+  this.displayString = function(pointInTime){
+    return "The " + this.dayOfMoonth(pointInTime) + "/" +this.daysInMoonth(pointInTime) + " day of the " + this.moonthOfYear(pointInTime) + "/" + this.moonthsInYear(pointInTime) + " Moonth of Year "
+  }
+
+
 }
 
-module.exports = EarthCycles
+module.exports = EarthCycles;
--- test/cycle_spec.js
+++ test/cycle_spec.js
@@ -11,54 +11,54 @@ describe('Cycle', function(){
 
   it('should count milliseconds into cycle', function(){
     var cycle = new Cycle(3, 10);
-    assert.equal(cycle.millisecondsIntoCycle(5),2)
-    assert.equal(cycle.millisecondsIntoCycle(13),0)
+    assert.equal(cycle.millisecondsIntoCycle(5),2);
+    assert.equal(cycle.millisecondsIntoCycle(13),0);
   })
 
   it('should show fraction into cycle', function(){
     var cycle = new Cycle(3, 10);
-    assert.equal(cycle.fractionOfCycle(8),0.5)
-    assert.equal(cycle.fractionOfCycle(13),0)
-    assert.equal(cycle.fractionOfCycle(18),0.5)
+    assert.equal(cycle.fractionOfCycle(8),0.5);
+    assert.equal(cycle.fractionOfCycle(13),0);
+    assert.equal(cycle.fractionOfCycle(18),0.5);
   })
 
   it('should show cycles since anchor', function(){
     var cycle = new Cycle(3, 10);
-    assert.equal(cycle.cyclesSinceAnchor(8), 0)
-    assert.equal(cycle.cyclesSinceAnchor(13), 1)
-    assert.equal(cycle.cyclesSinceAnchor(15), 1)
+    assert.equal(cycle.cyclesSinceAnchor(8), 0);
+    assert.equal(cycle.cyclesSinceAnchor(13), 1);
+    assert.equal(cycle.cyclesSinceAnchor(15), 1);
   })
 
   it('should show start of current cycle', function(){
     var cycle = new Cycle(3, 10);
-    assert.equal(cycle.startOfCurrentCycle(15), 13)
-    assert.equal(cycle.startOfCurrentCycle(7), 3)
+    assert.equal(cycle.startOfCurrentCycle(15), 13);
+    assert.equal(cycle.startOfCurrentCycle(7), 3);
   })
 
   it('should show start of next cycle', function(){
     var cycle = new Cycle(3, 10);
-    assert.equal(cycle.startOfNextCycle(15), 23)
-    assert.equal(cycle.startOfNextCycle(7), 13)
+    assert.equal(cycle.startOfNextCycle(15), 23);
+    assert.equal(cycle.startOfNextCycle(7), 13);
   })
 
   it('should show the first day of cycle', function(){
-    var anchor = new Date(2013,3,1,12,50,59,99)
-    var length = 60*60*24*10*1000 //ten days
-    var pointInTime = new Date(2013,3,3,15,50,59,99)
-    var nextDay = new Date(2013,3,2,0,0,0,0)
+    var anchor = new Date(2013,3,1,12,50,59,99);
+    var length = 60*60*24*10*1000; //ten days
+    var pointInTime = new Date(2013,3,3,15,50,59,99);
+    var nextDay = new Date(2013,3,2,0,0,0,0);
     var cycle = new Cycle(anchor.valueOf(), length);
-    assert.equal(cycle.firstDayStartOfCurrentCycle(pointInTime.valueOf()), nextDay.valueOf())  
+    assert.equal(cycle.firstDayStartOfCurrentCycle(pointInTime.valueOf()), nextDay.valueOf());
   })
 
   it('should show day of cycle', function(){
-    var anchor = new Date(2013,3,1,12,50,59,99)
-    var length = 60*60*24*10*1000 //ten days
-    var oneHourAfterAnchor = new Date(2013,3,1,13,50,59,99)
-    var oneDayAfterAnchor= new Date(2013,3,2,12,50,59,99)
+    var anchor = new Date(2013,3,1,12,50,59,99);
+    var length = 60*60*24*10*1000; //ten days
+    var oneHourAfterAnchor = new Date(2013,3,1,13,50,59,99);
+    var oneDayAfterAnchor= new Date(2013,3,2,12,50,59,99);
 
     var cycle = new Cycle(anchor.valueOf(), length);
-    assert.equal(cycle.dayOfCycle(oneHourAfterAnchor.valueOf()), 1)
-    assert.equal(cycle.dayOfCycle(oneDayAfterAnchor.valueOf()), 2) 
+    assert.equal(cycle.dayOfCycle(oneHourAfterAnchor.valueOf()), 1);
+    assert.equal(cycle.dayOfCycle(oneDayAfterAnchor.valueOf()), 2);
   })
 
 
